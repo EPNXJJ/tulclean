7869885587884613e9bc8a52ada584d92b7e7d32
EPNXJJ <szorcsik.istvan12@gmail.com>
2024-12-27 00:09:18 +0100

    Loading Screen
    Added loading screen

---
+++ Tulclean.hu/loading.js
@@ -0,0 +1,13 @@
+var loadingScreen = document.querySelector("#loader");
+var video = document.querySelector("#cleaning");
+var container = document.querySelector(".container");
+
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        loadingScreen.style.display = 'none';
+        video.classList.remove('hidden');
+        container.classList.remove('hidden');
+        video.classList.add('show');
+        container.classList.add('show');
+    }, 2000);
+});
